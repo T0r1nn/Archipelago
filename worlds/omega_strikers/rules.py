9e761cbfe22cf266ba7d73e2805015a12c646e5a
T0r1nn <44417700+T0r1nn@users.noreply.github.com>
2026-02-09 13:40:11 -0600

    finished switching to rule builder :D

--- worlds/omega_strikers/rules.py
+++ worlds/omega_strikers/rules.py
@@ -4,6 +4,7 @@ import string
 from BaseClasses import MultiWorld, CollectionState, ItemClassification, LocationProgressType
 from .options import OSOptions
 from typing import TYPE_CHECKING
+from rule_builder.rules import Has
 
 if TYPE_CHECKING:
     from . import OmegaStrikersWorld
@@ -16,4 +17,4 @@ def set_rules(os_world: 'OmegaStrikersWorld') -> None:
     TODO: add logic so that awakenings are only in logic when a character that can use them is unlocked
     """
 
-    multiworld.completion_condition[player] = lambda state: state.has("Victory", player)
+    os_world.set_completion_rule(Has("Victory"))
